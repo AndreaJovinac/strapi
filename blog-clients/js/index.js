0a8a77d489dc43216923744651e626e960cc963d
Andrea <45819894+AndreaJovinac@users.noreply.github.com>
2021-09-29 21:45:13 +0200

    finalisation title et content dynamqiue depuis l'aPI
    rendu du titre et du content dynamique finalisation de la journée du mercredi

--- blog-clients/js/index.js
+++ blog-clients/js/index.js
@@ -2,13 +2,15 @@ $(document).ready(function () {
   // X - Récuperer la liste des articles
 
   const api_url = "http://localhost:1337";
-
+  // on créer une constante et on met url dedans
   axios
-    .get(api_url + "/articles")
+    .get(api_url + "/articles") // à partir de la bilothèque axios
     .then((response) => {
-      const template = document.getElementById("articleCard").innerHTML;
+      // Tu lui fais une promesse de lui envoyer un truc
+      const template = document.getElementById("articleCard").innerHTML; // tu créer une constante qui s'appelle template dans lequel tu prendre l'ID articleCard
 
       const articlesHtml = response.data.map((article) => {
+        // tu créer une constante qui s'appelle articleHTML dans lequel tu mets un tableau l'ID articleCard
         return Mustache.render(template, article);
       });
 
@@ -16,7 +18,7 @@ $(document).ready(function () {
 
       start();
     })
-    .catch((error) => console.error(error));
+    .catch((error) => console.error(error)); // ça c'est pour gerer si il y a un erreur
 
   // X - Générer le HTML à partir du template Mustache
 
