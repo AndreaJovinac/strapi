d6ae8746f55f88f6685cf720786d450efe4e615e
Andrea <Andrea Jovinac>
2021-09-29 18:10:11 +0200

    installation strapi

---
+++ blog-admin/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'aff45a853bfd765b7ae562bb25de50a7'),
+    },
+  },
+});
---
+++ blog-admin/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'bf134b6b-965b-4e4b-b9fb-fd3b602eacd9'
+};
