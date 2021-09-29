890f702ee4b71567b21f03b60249fd2b37359a9f
Andrea <45819894+AndreaJovinac@users.noreply.github.com>
2021-09-29 21:25:29 +0200

    Change index.html / index.js
    Integration de l'api dans le blog-client

--- blog-clients/js/index.js
+++ blog-clients/js/index.js
@@ -1,3 +1,24 @@
 $(document).ready(function () {
-  // Page index is ready
+  // X - Récuperer la liste des articles
+
+  const api_url = "http://localhost:1337";
+
+  axios
+    .get(api_url + "/articles")
+    .then((response) => {
+      const template = document.getElementById("articleCard").innerHTML;
+
+      const articlesHtml = response.data.map((article) => {
+        return Mustache.render(template, article);
+      });
+
+      $(".grid-sizer").after(articlesHtml.join(""));
+
+      start();
+    })
+    .catch((error) => console.error(error));
+
+  // X - Générer le HTML à partir du template Mustache
+
+  // injecter le résultat dans la page
 });
