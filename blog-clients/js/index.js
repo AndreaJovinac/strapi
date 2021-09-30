b12d6e1320c1e28dc7701731a5301303098450cf
Andrea <45819894+AndreaJovinac@users.noreply.github.com>
2021-09-30 11:37:29 +0200

    Ajout de la date
    Formatage de la date

--- blog-clients/js/index.js
+++ blog-clients/js/index.js
@@ -21,8 +21,41 @@ $(document).ready(function () {
           .join(""); // <-- Joindre le tableau pour que ça devienne une chaine de cataractère
 
         // tu créer une constante qui s'appelle articleHTML dans lequel tu mets un tableau l'ID articleCard
-        console.log(article.categories[0].title); // <-- pour tester l'affichage des articles
-        console.log(article); // <-- pour tester l'affichage des articles
+        // console.log(article.categories[0].title); // <-- pour tester l'affichage des articles
+
+        /* */
+        /* DATE */
+        /* */
+
+        /* Pour formater des dates : https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Intl/DateTimeFormat/DateTimeFormat*/
+        const date = new Date(article.created_at); // créer une constante date et tu crées une nouvelle date et tu le mets
+        article.created_at = new Intl.DateTimeFormat("en", {
+          // on le met en anglais
+          // il faut que ce soit en US
+          dateStyle: "medium", // style des date
+        }).format(date);
+
+        /* C'est une autre proposition du prof
+        const date = new Date(article.creatd_at);
+        article.createad_at = new Intl.DateTimeFormat("en", {year: 'numeric', month:'short', day: 'numeric'}).format(date);
+        /*
+
+        /* C'est juste pour teste que je puisse bien récuperer la date de mon article */
+        /*console.log(
+          new Intl.DateTimeFormat("en-US", {
+            dateStyle: "medium",
+            formatMatcher: "basic",
+          }).format(date)
+        );*/
+
+        /* */
+        /* CONTENT */
+        /* */
+
+        /* */
+        /* IMAGE */
+        /* */
+        console.log(article.data.image.url);
         return Mustache.render(template, article);
       });
 
